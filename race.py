49a14f0ea4075496782b0cd2e29dfb0b807c3d9c
bryaneshen <47930778+bryaneshen@users.noreply.github.com>
2019-02-23 15:37:16 -0800

    Add files via upload

---
+++ race.py
@@ -0,0 +1,27 @@
+def charRace():
+    print("Please choose your race: Human, Elf, or Dwarf\n")
+    print("If you choose Human, all stats (wisdom, strength, dexterity and constitution) will +1.")
+    print("If you choose Elf, wisdom and dexterity will +2.")
+    print("If you choose Dwarf, constitution and strength will +2.\n")
+
+    boolean = True
+    while boolean == True:
+        x = input()
+        x = x.lower()
+        
+        if x == "human":
+            print("You chose Human!")
+            boolean = False
+        elif x == "elf":
+            print("You chose Elf!")
+            boolean = False
+        elif x == "dwarf":
+            print("You chose Dwarf!")
+            boolean = False
+        else:
+            print("Please enter a valid input.")
+
+charRace()
+
+#con: health and defence
+#dex: hit chances
