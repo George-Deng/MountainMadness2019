23b3172363b66dfccdeee366eef40dcb31daacc9
George-Deng <47930640+George-Deng@users.noreply.github.com>
2019-02-23 15:43:44 -0800

    Add files via upload

---
+++ modifier.py
@@ -0,0 +1,22 @@
+def mod(modtype):
+    if (modtype == 1):
+        modi = -5
+    elif (modtype == 2 | modtype ==3):
+        modi = -4
+    elif (modtype == 4 | modtype == 5):
+        modi = -3
+    elif (modtype == 6 | modtype == 7):
+        modi = -2
+    elif (modtype == 8 | modtype == 9):
+        modi = -1
+    elif (modtype == 10 | modtype == 11):
+        modi = 0
+    elif (modtype == 12 | modtype == 13):
+        modi = 1
+    elif (modtype == 14 | modtype == 15):
+        modi = 2
+    elif (modtype == 16 | modtype == 17):
+        modi = 3
+    elif (modtype == 18 | modtype == 19):
+        modi = 4
+    return modi
