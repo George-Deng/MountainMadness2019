925d241b57de1b9a82c690cfc5ab8089e4c1c856
George-Deng <47930640+George-Deng@users.noreply.github.com>
2019-02-23 18:03:08 -0800

    Add files via upload

---
+++ PCUpdated.py
@@ -0,0 +1,21 @@
+##stats
+wis = 14
+strt = 9
+dex = 13
+con = 15
+
+#mod
+wismod = 2
+strtmod = -1
+dexmod = 1
+conmod = 2
+
+hitpoints = 12 + conmod
+
+race = "Human"
+
+class1 = "Student"
+
+name = "A Real Human: Mark Zuckerberg" ##<-- default
+
+ac = 12
